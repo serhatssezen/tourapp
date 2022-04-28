25e4fbf5b3c9607f97d30807770806feab90de12
Aydın Serhat SEZEN <serhatssezen@gmail.com>
2022-04-28 13:14:28 +0300

    Initial Commit

---
+++ tourapp/tourappApp.swift
@@ -0,0 +1,17 @@
+//
+//  tourappApp.swift
+//  tourapp
+//
+//  Created by Aydın Serhat SEZEN on 28.04.2022.
+//
+
+import SwiftUI
+
+@main
+struct tourappApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
